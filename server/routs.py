8b7d569d4f77aaedd1f656d2bce74d19f9085eb0
DerTraurigeHund <luis.thomas.18.02.2005@gmail.com>
2024-08-02 20:38:54 +0200

    modified

--- server/routs.py
+++ server/routs.py
@@ -11,4 +11,11 @@ def load_routs(app, socketio):
     with open('server/routes.json', 'r') as f:
         routes = json.load(f)
 
+    for route in routes:
+        if route == 'socket':
+            for socket in routes[route]:
+                socketio.on_namespace(routes[route][socket])
+        else:
+            app.add_url_rule(routes[route]['path'], route, routes[route]['function'])
+
     
