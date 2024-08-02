fbff5e2acfe3ea264e842f260caf6f6bcc1efb6d
DerTraurigeHund <luis.thomas.18.02.2005@gmail.com>
2024-08-02 20:52:06 +0200

    renamed:    confic/mysql.json -> config/mysql.json 	modified:   server/routes.json

--- server/routs.py
+++ server/routs.py
@@ -4,8 +4,13 @@ import gunicorn
 import logging
 import json
 import os
+import colorama
+from config.session import get_session_permissions
 
-
+GREEN = colorama.Fore.GREEN
+YELLOW = colorama.Fore.YELLOW
+RED = colorama.Fore.RED
+RESET = colorama.Fore.RESET
 
 def load_routs(app, socketio):
     with open('server/routes.json', 'r') as f:
@@ -14,8 +19,27 @@ def load_routs(app, socketio):
     for route in routes:
         if route == 'socket':
             for socket in routes[route]:
-                socketio.on_namespace(routes[route][socket])
-        else:
-            app.add_url_rule(routes[route]['path'], route, routes[route]['function'])
+                try:
+                    name = routes[route][socket]['name']
+                    description = routes[route][socket]['description']
+                    function = eval(routes[route][socket]['function'])
 
-    
+                    socketio.on_event(socket, function)
+
+                    print(GREEN + f'Socket {name} loaded' + RESET)
+
+                except Exception as e:
+                    print(RED + f'Socket {name} not loaded: {str(e)}' + RESET)
+        elif route == 'html':
+            for html in routes[route]:
+                try:
+                    name = routes[route][html]['name']
+                    description = routes[route][html]['description']
+                    file = routes[route][html]['file']
+                    premission = routes[route][html]['required_permissions']
+                    dynamic = routes[route][html]['daynamic']
+
+                    if dynamic == [False]:
+                        @app.route(html)
+                        def {html}():
+
